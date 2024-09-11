a394c6ecb2327645d2f3d1189ad98cd4146d216e
Lucas Oliveira <lucasprogjr@gmail.com>
2024-09-11 07:53:23 -0300

    First Day!

---
+++ mySQL/cap1.sql
@@ -0,0 +1,11 @@
+/*DB hexon*/
+SELECT * FROM product;
+
+SELECT product_id, description, price, price * 1.07 AS taxed_price
+FROM product;
+
+SELECT product_id, description, price AS untaxed_price, price * 1.07 AS taxed_price
+FROM product;
+
+SELECT product_id, description, price AS untaxed_price, ROUND(price * 1.07,2) AS taxed_price
+FROM product;
