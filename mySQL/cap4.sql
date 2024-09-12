867463f7cc6bd71209c1ca85394e1ff28a6dd51a
Lucas Oliveira <lucasprogjr@gmail.com>
2024-09-11 21:49:37 -0300

    Day2: Learn About Concat

--- mySQL/cap4.sql
+++ mySQL/cap4.sql
@@ -9,3 +9,10 @@ FROM product;
 
 SELECT product_id, description, price AS untaxed_price, ROUND(price * 1.07,2) AS taxed_price
 FROM product;
+
+SELECT name, city || ', ' || state AS location
+FROM customer;
+
+SELECT name, street_address || ', ' || city || '-' || state AS location
+FROM customer;
+
