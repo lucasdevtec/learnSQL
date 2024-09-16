9786b4aef6ff92a836ef56ec26b0a146864ac45e
Lucas Oliveira <lucasprogjr@gmail.com>
2024-09-16 08:49:42 -0300

    Day5: Group by, Order by e Having

--- mySQL/cap6.sql
+++ mySQL/cap6.sql
@@ -62,3 +62,18 @@ max(precipitation) AS max_precip
 FROM station_data
 WHERE year >= 2000
 GROUP BY 1;
+
+SELECT year, month,
+sum(snow_depth) AS sumSnowDepth,
+sum(precipitation) AS total_precip,
+max(precipitation) AS max_precip
+FROM station_data
+WHERE year >= 2000
+GROUP BY year, month
+ORDER BY year ASC, month;
+
+SELECT station_number
+FROM station_data;
+
+SELECT DISTINCT station_number
+FROM station_data;
