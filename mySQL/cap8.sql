c7c47654db3c1205ef457409e6134d31f6e48762
Lucas <lucasprogjr@gmail.com>
2024-09-17 16:59:15 -0300

    Day6: Left, Outer e Right Join

--- mySQL/cap8.sql
+++ mySQL/cap8.sql
@@ -39,3 +39,38 @@ FROM customer LEFT JOIN customer_order
 ON customer.customer_id = customer_order.customer_id
 GROUP BY 1;
 
+SELECT *,
+sum(order_qty * price) AS total
+FROM customer
+INNER JOIN customer_order
+ON customer.customer_id = customer_order.customer_id
+INNER JOIN product
+ON product.product_id = customer_order.product_id
+GROUP BY customer.customer_id;
+
+SELECT *,
+sum(order_qty * price) AS total
+FROM customer
+INNER JOIN customer_order
+ON customer.customer_id = customer_order.customer_id
+INNER JOIN product
+ON product.product_id = customer_order.product_id;
+
+SELECT *,
+sum(order_qty * price) AS total
+FROM customer
+LEFT JOIN customer_order
+ON customer.customer_id = customer_order.customer_id
+LEFT JOIN product
+ON product.product_id = customer_order.product_id
+GROUP BY customer.customer_id;
+
+SELECT *,
+coalesce(sum(order_qty * price), 0) AS total
+FROM customer
+LEFT JOIN customer_order
+ON customer.customer_id = customer_order.customer_id
+LEFT JOIN product
+ON product.product_id = customer_order.product_id
+GROUP BY customer.customer_id;
+
